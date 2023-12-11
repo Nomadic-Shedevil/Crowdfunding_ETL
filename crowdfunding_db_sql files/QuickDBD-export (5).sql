663a5ea9082fa12db27b821ed3d4300599ae4b5a
Nomadic-Shedevil <jessica.baggett@yahoo.com>
2023-12-10 20:09:05 -0600

    ERD diagram and sql database

---
+++ crowdfunding_db_sql files/QuickDBD-export (5).sql
@@ -0,0 +1,59 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "category" (
+    "category" VARCHAR(4)   NOT NULL,
+    "category_name" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory" VARCHAR(8)   NOT NULL,
+    "subcategory_name" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" VARCHAR(6)   NOT NULL,
+    "first_name" CHAR(30)   NOT NULL,
+    "last_name" CHAR(30)   NOT NULL,
+    "email" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" VARCHAR(6)   NOT NULL,
+    "company_name" VARCHAR(50)   NOT NULL,
+    "description" VARCHAR(60)   NOT NULL,
+    "goal" INT   NOT NULL,
+    "pledged" INT   NOT NULL,
+    "outcome" VARCHAR(10)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" CHAR(3)   NOT NULL,
+    "currency" CHAR(3)   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(4)   NOT NULL,
+    "subcategory_id" VARCHAR(8)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory");
+
